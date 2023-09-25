ee83e01401fb6767681aed1de61895ee5f77e8f4
Lisa <llvvss@icloud.com>
2023-09-25 15:57:52 -0600

    added comments to make statements more clear

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def isValid(s: str) -> bool:
     stack = []
     closeToOpen = {")": "(", "]": "[", "}": "{"}
     for c in s:
-        if c in closeToOpen:
+        if c in closeToOpen:    # if c is a closing paren of some sort
+            # stack[-1] refers to the last element on the stack, without popping it
+            # "if stack" means if stack is not empty
+            # "stack[-1] == closeToOpen[c]" << last element on stack has
+            #   correct match (to c) in the dictionary key-value pairs
             if stack and stack[-1] == closeToOpen[c]:
-                stack.pop()
+                stack.pop()     # remove last element on stack, since it matches c
             else:
                 return False
         else:
